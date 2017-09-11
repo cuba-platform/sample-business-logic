8ab3041700eff11276e409431777cefd2c10f1a8
Konstantin Krivopustov <krivopustov@haulmont.com>
2017-09-11 14:44:16 +0400

    No such screen" when clicking lookup or search in the "Customer" field of the order editor. Fixes #6

--- modules/web/src/com/company/sample/web/order/OrderEdit.java
+++ modules/web/src/com/company/sample/web/order/OrderEdit.java
@@ -2,6 +2,18 @@ package com.company.sample.web.order;
 
 import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.company.sample.entity.Order;
+import com.haulmont.cuba.gui.components.LookupPickerField;
+
+import javax.inject.Named;
+import java.util.Map;
 
 public class OrderEdit extends AbstractEditor<Order> {
+
+    @Named("fieldGroup.customer")
+    private LookupPickerField customerField;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        customerField.removeAllActions();
+    }
 }
