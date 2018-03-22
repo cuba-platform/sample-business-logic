763b30404ade083e8b915bd5f7b2654d8db47a6e
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-03-22 11:06:48 +0400

    Replace usage of AppContext.Listener

--- modules/core/src/com/company/sample/core/AppLifecycle.java
+++ modules/core/src/com/company/sample/core/AppLifecycle.java
@@ -1,8 +1,11 @@
 package com.company.sample.core;
 
-import com.haulmont.cuba.core.sys.AppContext;
+import com.haulmont.cuba.core.global.Events;
+import com.haulmont.cuba.core.sys.events.AppContextInitializedEvent;
 import com.haulmont.cuba.core.sys.listener.EntityListenerManager;
 import com.haulmont.cuba.security.entity.User;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
@@ -11,28 +14,14 @@ import javax.inject.Inject;
  * Registers an entity listener for the User entity on application startup.
  */
 @Component("sample_AppLifecycle")
-public class AppLifecycle implements AppContext.Listener {
+public class AppLifecycle {
 
     @Inject
     private EntityListenerManager entityListenerManager;
 
-    public AppLifecycle() {
-        // Add itself to the collection of lifecycle listeners
-        AppContext.addListener(this);
-    }
-
-    /**
-     * Invoked by the framework on application startup.
-     */
-    @Override
-    public void applicationStarted() {
+    @EventListener(AppContextInitializedEvent.class) // notify after AppContext is initialized
+    @Order(Events.LOWEST_PLATFORM_PRECEDENCE + 100)  // run after all platform listeners
+    public void initEntityListeners() {
         entityListenerManager.addListener(User.class, "sample_UserEntityListener");
-    }
-
-    /**
-     * Invoked by the framework on application shutdown.
-     */
-    @Override
-    public void applicationStopped() {
     }
 }
